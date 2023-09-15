8c98a6e8b1a113731b674290c3d4b4e3db22d437
roba <10408936+roba-adnew@users.noreply.github.com>
2023-09-15 12:42:55 -0700

    Updated RPS with Buttons
    The RPS site has been updated with buttons to get player selection as
    as opposed to prompting the user for text input. Currently the game
    will record results to the console.
     Changes to be committed:
    	modified:   index.html
    	modified:   rps.js

--- rps.js
+++ rps.js
@@ -1,30 +1,20 @@
 // The game will be defined as a main function that has no inputs
-function playRound() {
+    // Updating the player selection to capture what they were going for through
+    // button clicks, instead of a prompt. 
+ function playRound(playerChoice) {
     // First I will define the parameters that you can use to play the game
     const hands = ["rock", "paper", "scissors"];
-    const handsAbbrv = ["r", "p", "s"];
 
-    // Below will function that has a computer randomly selects a hand from above 
+    // Below will function with computer randomly selects a hand from above 
     function getComputerChoice() {
         let n = Math.floor(Math.random() * 2);
         return hands[n];
     }
 
-    // Below will be code to accept a user's input, for now they can either write out the full word  of a hand or the
-    // first letter of the hand. If they don't provide either, we will return that the hand is invalid 
-    let playerChoice = prompt("Rock, Paper Scissors, Shoot: ");
-    playerChoice = playerChoice.toLowerCase();
-
-    if (!hands.includes(playerChoice) && !handsAbbrv.includes(playerChoice)) {
-        return "You didn't throw a valid hand...come on";
-    }
-
-    // Below is a function or a switch-case statement that evaluates the hands provided and return's text showing 
-    // whether or not you've won or tied and what hands were played
+    // Below is a function or a switch-case statement that evaluates the hands
+    // provided and return's text showing whether or not you've won or tied and 
+    // what hands were played
     function eval(playerSelection, computerSelection) {
-        if (playerSelection.length == 1) {
-            playerSelection = hands[handsAbbrv.indexOf(playerSelection)];
-        }
 
         const winner_message = "You win! "+ playerSelection + " beats " + computerSelection;
         const loser_message = "You lost! "+ computerSelection + " beats " + playerSelection;
@@ -83,24 +73,33 @@ function playRound() {
             
             }
         }
-    
+
     return eval(playerChoice, getComputerChoice());
     }
 
-// The game will consist of 5 rounds, in which we will print the result of each round and how well you did 
-function game() {
-    let playerWins = 0;
-    let compWins = 0;
+// This will return a nodelist of all the buttons 
+const buttons = document.querySelectorAll("button");
 
-    for (let i = 0; i < 5; i++) {
-        let result = playRound();
-        playerWins += result;
-    }
+// We will turn this nodelist into an array and then call a function on each
+buttons.forEach(button => 
+    button.addEventListener("click", function(event) {playRound(button.value);
+    }));
 
-    return "You won " + playerWins + " round(s)!" 
-    }
+// The game will consist of 5 rounds, in which we will print the result of each 
+//  round and how well you did 
+// function game() {
+//     let playerWins = 0;
+//     let compWins = 0;
 
-console.log(game()); 
+//     for (let i = 0; i < 5; i++) {
+//         let result = playRound();
+//         playerWins += result;
+//     }
+
+//     return "You won " + playerWins + " round(s)!" 
+//     }
+
+
 
 
 
